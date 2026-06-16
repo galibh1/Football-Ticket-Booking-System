dee782944385c272157733b33407c57b60f5918c
galibh1 <galibh233@gmail.com>
2026-06-16 21:34:30 +0600

    next two

--- QUERY.sql
+++ QUERY.sql
@@ -3,9 +3,33 @@ SELECT match_id, fixture, base_ticket_price
 FROM matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
-  
+
 --2
   SELECT user_id, full_name, email
 FROM users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
+
+   --3
+   SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required')
+        AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+
+--4
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u
+    ON b.user_id = u.user_id
+INNER JOIN Matches m
+    ON b.match_id = m.match_id;
+
