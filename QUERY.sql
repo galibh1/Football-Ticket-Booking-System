389d36ed749a41cffe928383c4636c733d5633eb
galibh1 <galibh233@gmail.com>
2026-06-16 20:13:49 +0600

    second query

--- QUERY.sql
+++ QUERY.sql
@@ -3,3 +3,9 @@ SELECT match_id, fixture, base_ticket_price
 FROM matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
+  
+--2
+  SELECT user_id, full_name, email
+FROM users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
